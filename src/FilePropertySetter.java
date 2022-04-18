c368468e132b7c8fb83f69019caf27c822d32e06
AwesomenessReborn <hareee234@gmail.com>
2022-04-18 09:31:30 -0700

    last changes

--- src/FilePropertySetter.java
+++ src/FilePropertySetter.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,56 +9,69 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
 public class FilePropertySetter {
     public final static String fileSeparator = System.getProperty("file.separator");
     public final static String lineSeparator = System.getProperty("line.separator");
     public final static String userDir = System.getProperty("user.dir");
     
     public static void main(String[] args) throws Exception {
-        // program to change copy all "modified dates" into the "created date". 
         
-        
-        //  changeable field to match folder path. 
-        File folderPath = new File("C:\\Users\\rams3\\Desktop\\File Property Setter\\data\\test"); 
-        // NOTE all files within folder will be changed to designated time change. 
-        
-        String[] s = folderPath.list(); 
-        
-        for (String string : s) {
-            File fileReference = new File(folderPath, string); 
+        try {
+            JFileChooser chooser = new JFileChooser(userDir); 
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
             
-            if (fileReference.isFile()) {
-                // Code to perform the task: copy the modified date into the created date 
+            int returnVal = chooser.showOpenDialog(null); 
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                String folderString = chooser.getSelectedFile().getPath(); 
                 
+                File folderPath = new File(folderString); 
                 
-                String fileName = fileReference.getPath(); 
-                System.out.println("\n now processing file: " + fileName);
+                String[] s = folderPath.list(); 
                 
-                Path file = Paths.get(fileName);
-                BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
-                FileTime first = attr.lastModifiedTime();
-                
-                // print path of which file is being changed. 
-
-
-                // print original last modified time
-                System.out.println("[original] getting modified time into created time:" + first);
-                
-                // NOTE TODO this is the new modified date for now, will change later change created date. 
-                // LocalDate newLocalDate = LocalDate.of(1998, 9, 30);
-                // // convert LocalDate to instant, need time zone
-                // Instant instant = newLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant();
-
-                // convert instant to filetime
-                // update last modified time
-                Files.setAttribute(file, "creationTime", first); 
-                
-                // read last modified time again
-                FileTime newCreatedTime = Files.readAttributes(file,
-                BasicFileAttributes.class).creationTime();
-                System.out.println("[updated] creationTime:" + newCreatedTime); 
-
+                for (String string : s) {
+                    File fileReference = new File(folderPath, string); 
+                    
+                    if (fileReference.isFile()) {
+                        String fileName = fileReference.getPath(); 
+                        System.out.println("\n***now processing file: " + fileName);
+                        
+                        Path file = Paths.get(fileName);
+                        BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+                        FileTime first = attr.lastModifiedTime();
+                        
+                        System.out.println("[original] getting modified time into created time:" + first);
+                        
+                        Files.setAttribute(file, "creationTime", first); 
+                        
+                        // read last modified time again
+                        FileTime newCreatedTime = Files.readAttributes(file,
+                        BasicFileAttributes.class).creationTime();
+                        System.out.println("[updated] creationTime:" + newCreatedTime); 
+                        
+                    }
+                }
+            } else {
+                int a = JOptionPane.showConfirmDialog(null, "ERR OCCURED, restart?", "Property Setter", JOptionPane.ERROR_MESSAGE); 
+                if (a == JOptionPane.YES_OPTION) {
+                    main(null); 
+                } else {
+                    return; 
+                }                
             }
+        } catch (Exception e) {
+            int a = JOptionPane.showConfirmDialog(null, "Err occured with file chooser, restart?", "Property Setter", JOptionPane.ERROR_MESSAGE); 
+            e.printStackTrace();
+            
+            if (a == JOptionPane.YES_OPTION) {
+                main(null); 
+            } else {
+                return; 
+            }    
         }
+        
+        
     }
 }
